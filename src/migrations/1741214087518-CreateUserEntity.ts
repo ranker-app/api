44a4ff822de6c275b2fa6c30de830f18eb1e4127
kaiovbraga <kaiovbraga2001@gmail.com>
2025-03-05 20:50:07 -0300

    chore: Using SQL Highlighting extension

--- src/migrations/1741214087518-CreateUserEntity.ts
+++ src/migrations/1741214087518-CreateUserEntity.ts
@@ -2,25 +2,23 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateUserEntity1741214087518 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `
-      CREATE TABLE \`user\` (
-        \`id\` INT NOT NULL AUTO_INCREMENT,
-        \`name\` VARCHAR(255) NOT NULL,
-        \`nick_name\` VARCHAR(255) NOT NULL,
-        \`email\` VARCHAR(255) NOT NULL,
-        \`create_user\` VARCHAR(50),
-        \`create_dt\` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        \`update_user\` VARCHAR(50),
-        \`update_dt\` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    await queryRunner.query(/* sql */ `
+      CREATE TABLE user (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(255) NOT NULL,
+        nick_name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        create_user VARCHAR(50),
+        create_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        update_user VARCHAR(50),
+        update_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-        PRIMARY KEY (\`id\`)
+        PRIMARY KEY (id)
       );
-      `,
-    );
+      `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE \`user\`;`);
+    await queryRunner.query(/* sql */ `DROP TABLE user;`);
   }
 }
